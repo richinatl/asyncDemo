11fc0b3738ef13107a0f24eab62e8f931990c6ba
Richard Murphy <richinatl@gmail.com>
2022-12-08 11:55:03 -0500

    adding basic functionality

---
+++ script.js
@@ -0,0 +1,32 @@
+const p = document.querySelector("p");
+
+setTimeout(function () {
+  p.textContent = "Our names are Rich and Mace, nice to meet you!";
+}, 3000);
+
+p.style.color = "blue";
+
+var count = 0;
+//  Select increment and decrement button elements
+var incrementEl = document.querySelector("#increment");
+var decrementEl = document.querySelector("#decrement");
+var countEl = document.querySelector("#count");
+
+// Updates count on page
+function setCounterText() {
+  countEl.textContent = count;
+}
+// Attach event listener to increment button element
+incrementEl.addEventListener("click", function () {
+  count++;
+  setCounterText();
+});
+
+// Attach event listener to decrement button element
+decrementEl.addEventListener("click", function () {
+  // Action will fire if count is greater than  0
+  if (count > 0) {
+    count--;
+    setCounterText();
+  }
+});
